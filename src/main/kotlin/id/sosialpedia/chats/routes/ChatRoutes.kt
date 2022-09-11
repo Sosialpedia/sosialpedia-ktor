138c7c76a39787b65ced942fd2741e393dadd4c4
Samuel Mareno <samuelmareno777@gmail.com>
2022-09-12 00:15:43 +0700

    websocket check if userId is exist or not

--- src/main/kotlin/id/sosialpedia/chats/routes/ChatRoutes.kt
+++ src/main/kotlin/id/sosialpedia/chats/routes/ChatRoutes.kt
@@ -4,6 +4,7 @@ import id.sosialpedia.chats.rooms.RoomController
 import id.sosialpedia.chats.rooms.domain.RoomsDataSource
 import id.sosialpedia.chats.routes.model.CreateMessageRequest
 import id.sosialpedia.chats.util.UserNotInRoom
+import id.sosialpedia.users.domain.UserRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -23,10 +24,15 @@ fun Route.configureChats() {
 
     val roomController by inject<RoomController>()
     val roomsDataSource by inject<RoomsDataSource>()
+    val userRepository by inject<UserRepository>()
     webSocket("/chat") {
 
         val userId = call.parameters["userId"] ?: throw IllegalArgumentException("userId can't be empty")
         println("Adding user! with userId: $userId and socket: $this")
+
+        val userIdIsNotExist = userRepository.getUserById(userId) == null
+
+        if (userIdIsNotExist) return@webSocket
         try {
             roomController.onJoin(userId, this)
             incoming.consumeEach { frame ->
